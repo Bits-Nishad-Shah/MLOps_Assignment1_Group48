a02b289c0de54c8505c3ce8270052eb216b97e60
Nishad Shah <nisshah@microsoft.com>
2024-08-03 11:44:21 +0530

    bug fixes

--- M2-Process-Tooling/main.py
+++ M2-Process-Tooling/main.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 # Load dataset
-iris = pd.read_csv("iris.csv")           
+iris = pd.read_csv("iris_full.csv")           
 X = iris.drop("target", axis=1)
 y = iris["target"]
 
@@ -15,7 +15,7 @@ y = iris["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Set the MLflow tracking server
-experiment_id = mlflow.create_experiment("Trimmed Dataset")
+experiment_id = mlflow.create_experiment("Full Dataset")
 experiment = mlflow.get_experiment(experiment_id)
 
 # Model 1: Random Forest
@@ -39,10 +39,19 @@ with mlflow.start_run(run_name="Random Forest 10 2", experiment_id=experiment.ex
     mlflow.log_metric("accuracy", accuracy)
     
     # Log model
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, "fd_model1")
     
     print(f"Model with n_estimators={n_estimators} and max_depth={max_depth} has accuracy: {accuracy}")
 
+    # Define the model name
+    model_name = "fd_random_forest_model1"
+    
+    # Register the model
+    run_id = mlflow.active_run().info.run_id
+    model_uri = f"runs:/{run_id}/fd_random_forest_model1"
+    registered_model = mlflow.register_model(model_uri=model_uri, name=model_name)
+    print(f"Model registered with name: {registered_model.name} and version: {registered_model.version}")
+    
 
 # Model 2: Random Forest
 with mlflow.start_run(run_name="Random Forest 50 5", experiment_id=experiment.experiment_id):
@@ -65,9 +74,10 @@ with mlflow.start_run(run_name="Random Forest 50 5", experiment_id=experiment.ex
     mlflow.log_metric("accuracy", accuracy)
     
     # Log model
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, "fd_model2")
     
     print(f"Model with n_estimators={n_estimators} and max_depth={max_depth} has accuracy: {accuracy}")
+
 
 
 # Model 3: Random Forest
@@ -91,7 +101,6 @@ with mlflow.start_run(run_name="Random Forest 100 10", experiment_id=experiment.
     mlflow.log_metric("accuracy", accuracy)
     
     # Log model
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, "fd_model3")
     
     print(f"Model with n_estimators={n_estimators} and max_depth={max_depth} has accuracy: {accuracy}")
-
--- M2-Process-Tooling/temp.py
+++ M2-Process-Tooling/temp.py
@@ -8,10 +8,6 @@ iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 iris_df['target'] = iris.target
 
 # Save to CSV
-iris_df.to_csv('iris.csv', index=False)
+iris_df.to_csv('iris_full.csv', index=False)
 
 
-# Run experiments with different parameters
-train_model(10, 2)
-train_model(50, 5)
-train_model(100, 10)
